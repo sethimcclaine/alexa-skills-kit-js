0f7be1d81194e898b7a7859017260706b52e15c2
Seth McClaine <smcclaine@smcclaine-mbr.f4tech.com>
2016-01-05 14:59:02 -0700

    add a node script that can be run while programming that will rezip contents on each save 	new file:   watcher.js

---
+++ samples/flowDock/src/watcher.js
@@ -0,0 +1,42 @@
+/**
+ * @author Seth M.
+ * 1) `npm install chokidar`
+ * 2) Specifiy files to watch in `files` array
+ */
+var files = ['index.js'];
+/*
+ * 3) Specify what you want to happen on change
+ */
+var onChange = function(path) { 
+    log("\n\n");
+    log('--------------------------');
+    //zipping all files that end with .js
+    exec("zip Archive *.js; ls -l;", function (error, stdout, stderr) {
+        sys.print('stdout: ' + stdout);
+    });
+
+    log('File', path, 'has been changed'); 
+};
+/**
+ * 4) run `node watcher.js` to start
+ * `ctrl c` to stop
+ */
+
+var chokidar = require('chokidar');
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+
+var watcher = chokidar.watch('file, dir, or glob', {
+    ignored: /[\/\\]\./, persistent: true
+});
+
+var log = console.log.bind(console);
+
+//options ['add', 'addDir', 'change', 'unlink', 'unlinkDir', 'error', 'ready', 'raw']
+watcher
+.on('change', onChange)
+
+for (var i = 0; i < files.length; i++) {
+    watcher.add(files[i]);
+}
